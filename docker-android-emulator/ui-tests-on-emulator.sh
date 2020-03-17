46fdf08b9865ec9d39d7c78fd23354129d46552f
Sanket Bhat <sanket.r.bhat.u@gmail.com>
2020-03-17 13:18:01 +0530

    Update ui-tests-on-emulator.sh

--- docker-android-emulator/ui-tests-on-emulator.sh
+++ docker-android-emulator/ui-tests-on-emulator.sh
@@ -6,7 +6,7 @@ set -eu
 START=`date +%s` > /dev/null
 
 echo no | $ANDROID_HOME/tools/bin/avdmanager create avd --force -n test android-21 --abi default/armeabi-v7a
-$ANDROID_HOME/tools/android list avd
+$ANDROID_HOME/tools/bin/avdmanager list avd
 $ANDROID_HOME/tools/emulator64-arm -avd test -no-window -no-boot-anim -no-audio -verbose &
 wait-for-emulator
 unlock-emulator-screen
